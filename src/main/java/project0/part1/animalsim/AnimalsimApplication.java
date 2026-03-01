6c54aa071d5629ae5776a6e26256cdcc1ba9bf9e
Rose, Ian Christian <ianrose@iu.edu>
2026-03-01 00:24:51 -0500

    part1 start

---
+++ src/main/java/project0/part1/animalsim/AnimalsimApplication.java
@@ -0,0 +1,13 @@
+package project0.part1.animalsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalsimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project0/part1/animalsim/AnimalsimApplicationTests.java
@@ -0,0 +1,13 @@
+package project0.part1.animalsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalsimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
