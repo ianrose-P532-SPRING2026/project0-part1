5183bdc146388a2c3881354846e90f492576ce2f
Rose, Ian Christian <ianrose@iu.edu>
2026-03-01 02:41:56 -0500

    part 1 done

--- src/main/java/animalsim/AnimalSimApplication.java
+++ src/main/java/animalsim/AnimalSimApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AnimalsimApplication {
+public class AnimalSimApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AnimalsimApplication.class, args);
+		SpringApplication.run(AnimalSimApplication.class, args);
 	}
 
 }
---
+++ src/main/java/animalsim/controllers/AnimalController.java
@@ -0,0 +1,51 @@
+package animalsim.controllers;
+
+import animalsim.model.AnimalData;
+import animalsim.repository.AnimalRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/animals")
+public class AnimalController {
+    private AnimalRepository animalRepository;
+
+    public AnimalController(AnimalRepository animalRepository){
+        this.animalRepository = animalRepository;
+    }
+
+    @PostMapping
+    public boolean add(@RequestBody AnimalData data){
+        try {
+            return animalRepository.add(data);
+        } catch (IOException e){
+            return false;
+        }
+    }
+
+    @GetMapping
+    public List<AnimalData> findAll(){
+        try {
+            return animalRepository.findAll();
+        } catch (IOException e){
+            return null;
+        }
+    }
+
+    @GetMapping("/search")
+    public List<AnimalData> search(@RequestParam String name,
+                                   @RequestParam String picture,
+                                   @RequestParam String location){
+        try {
+            System.out.println(name);
+            System.out.println(picture);
+            System.out.println(location);
+
+            return animalRepository.find(name, picture, location);
+        } catch (IOException e){
+            return null;
+        }
+    }
+}
